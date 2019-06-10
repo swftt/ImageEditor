fa1cafd5859c9a05b7c95f09d3e21bb51444dc41
Volodimir Chubiuk <vaseho1337@gmail.com>
2019-06-10 13:23:40 +0300

    Flip image

---
+++ FlipImage/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using PluginFramework;
+namespace FlipImage
+{
+    public class FlipImage : IFilter
+    {
+        public string Name => "Flip Image";
+
+        public Image RunPlungin(Image src)
+        {
+            Bitmap bitmap = new Bitmap(src);
+            Bitmap newBitmap = new Bitmap(src);
+            for (int row = 0; row < bitmap.Height; ++row)
+            {
+                for (int col = 0; col < bitmap.Width; ++col)
+                {
+                    newBitmap.SetPixel(bitmap.Width - col - 1, bitmap.Height - row - 1, bitmap.GetPixel(col,row));
+                }
+            }
+            bitmap.Dispose();
+            return newBitmap;
+        }
+    }
+}
